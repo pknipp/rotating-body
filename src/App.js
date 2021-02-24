f2d7d44efa7fc5ce0d13da75e481d60cf9b5e46d
Peter Knipp <peteraknipp@gmail.com>
2021-02-24 16:32:18 -0500

    Get rotation to work.

--- src/App.js
+++ src/App.js
@@ -35,19 +35,30 @@ const App = () => {
 
     const nx = 1400;
     const ny = 700;
-    const d = 10;
+    const d = 20;
+    const th1 = 0.2;
+    const th2 = 0.4;
+    const th3 = 0.6;
+
+    const mat1 = zRot(th1);
+    const mat2 = xRot(th2);
+    const mat3 = zRot(th3);
+
     const xys = []
     for (let i = 0; i < 2; i++) {
-        let x = (1 + 2 * i) * (nx / 4);
+        let x = (-1 + 2 * i) * (nx / 4);
         for (let j = 0; j < 2; j++) {
-            let y = (1 + 2 * j) * (ny / 4);
+            let y = (-1 + 2 * j) * (ny / 4);
             xys.push([x, y, 0]);
         }
     }
-    console.log(xys);
     return (
         <div className="container" style={{height:`${ny}px`, width:`${nx}px`}}>
-            {xys.map(xy => <Dot x={xy[0]} y={xy[1]} z={xy[2]} d={d} />)}
+            {xys.map(xy => <Dot x={xy[0] + nx / 2} y={xy[1] + ny / 2} z={xy[2]} d={d} />)}
+            {xys.map(xy => {
+                let newXy = mult1(mat2, xy);
+                return <Dot x={newXy[0] + nx / 2} y={newXy[1] + ny / 2} z={newXy[2]} d={d} dashed={true} />
+            })}
         </div>
     )
 }
--- src/Dot.js
+++ src/Dot.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-const Dot = ({ x, y, z, d }) => {
-    console.log(x, y, d);
+const Dot = ({ x, y, z, d, dashed }) => {
     return (
         <div className="dot" style={{
             width:`${d}px`,
             height:`${d}px`,
             left: `${x - d / 2}px`,
             top: `${y - d / 2}px`,
+            borderStyle: `${dashed ? "dashed" : "solid"}`
         }}/>
     )
 }
