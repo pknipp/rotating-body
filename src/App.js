ad3cfbbb70a6af63bf23e41398ec15e06c2c4f47
Peter Knipp <peteraknipp@gmail.com>
2021-02-24 21:21:54 -0500

    Tighten up code.

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import Dot from "./Dot";
 import Slider from "./Slider";
 
 const App = () => {
-    const getSet = {th0: useState(0), th1: useState(0), th2: useState(0)};
-    // const {th1, th2, th3} =
-    const sliderHandler = e => getSet[e.target.name][1](Number(e.target.value));
+    const getSet = [useState(0), useState(0), useState(0)];
+    const sliderHandler = e => {
+        console.log(e.target.name);
+        getSet[Number(e.target.name)][1](Number(e.target.value));
+    }
     const mult2 = (arr1, arr2) => {
         let arr3 = [];
         for (let i = 0; i < 3; i++) {
@@ -20,8 +22,6 @@ const App = () => {
         return arr3;
     }
     const mult1 = (arr, vec) => {
-        // console.log("arr = ", arr);
-        console.log("vec = ", vec);
         let vec2 = [];
         for (let i = 0; i < 3; i++) {
             let elem = 0;
@@ -39,15 +39,10 @@ const App = () => {
         return [[1, 0, 0], [0, c, s], [0, -s, c]];
     }
 
-
     const nx = 1200;
     const ny = 700;
     const nz = ny;
     const d = 50;
-
-    // const mat0 = zRot(th0);
-    // const mat1 = xRot(th1);
-    // const mat2 = zRot(th2);
 
     const xyzs = []
     for (let i = 0; i < 2; i++) {
@@ -60,12 +55,12 @@ const App = () => {
             }
         }
     }
-    let newXyzs = xyzs.map(xyz => mult1(mult2(mult2(zRot(getSet.th0[0]), xRot(getSet.th1[0])),zRot(getSet.th2[0])), xyz));
+    let newXyzs = xyzs.map(xyz => mult1(mult2(mult2(zRot(getSet[0][0]), xRot(getSet[1][0])),zRot(getSet[2][0])), xyz));
     return (
         <>
-        <Slider name={"th0"} maxVal={2 * Math.PI} stepSize={0.1} quantity={getSet.th0[0]} handler={sliderHandler} />
-        <Slider name={"th1"} maxVal={2 * Math.PI} stepSize={0.1} quantity={getSet.th1[0]} handler={sliderHandler} />
-        <Slider name={"th2"} maxVal={2 * Math.PI} stepSize={0.1} quantity={getSet.th2[0]} handler={sliderHandler} />
+        <Slider n={0} maxVal={2 * Math.PI} stepSize={0.1} quantity={getSet[0][0]} handler={sliderHandler} />
+        <Slider n={1} maxVal={2 * Math.PI} stepSize={0.1} quantity={getSet[1][0]} handler={sliderHandler} />
+        <Slider n={2} maxVal={2 * Math.PI} stepSize={0.1} quantity={getSet[2][0]} handler={sliderHandler} />
         <div className="container" style={{height:`${ny}px`, width:`${nx}px`}}>
             {   xyzs.map(xyz => <Dot x={xyz[0] + nx / 2} y={xyz[1] + ny / 2} d={d} dashed={true} />)}
             {newXyzs.map(newXyz => <Dot x={newXyz[0] + nx / 2} y={newXyz[1] + ny / 2} d={d} />)}
--- src/Slider.js
+++ src/Slider.js
@@ -1,17 +1,14 @@
 import React from "react";
 
-const Slider = ({name, maxVal, stepSize, quantity, handler}) => (
-    <>
-    {name}
+const Slider = ({n, maxVal, stepSize, quantity, handler}) => (
     <input
         type="range"
         onChange={handler}
-        name={`${name}`}
+        name={`${n}`}
         min="0"
         max={`${maxVal}`}
         step={`${stepSize}`}
         value={quantity}
     />
-    </>
 )
 export default Slider;
